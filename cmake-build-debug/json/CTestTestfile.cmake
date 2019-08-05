8b9ee4ca58bfd438780b8e01b8084be3c7ed45c3
Calogero Carrabbotta <ccarrabbotta@topcon.com>
2019-08-05 18:00:20 +0200

    Banished files and renamed subdir

--- cmake-build-debug/json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kalos/CLionProjects/Voice_for_VLC/json
-# Build directory: /home/kalos/CLionProjects/Voice_for_VLC/cmake-build-debug/json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
